11d0d86dd0d8ed04b9a42794d81f0622c0db7171
darashi <darashi003@gmail.com>
2022-12-19 15:28:24 +0300

    Changed SeatInfo

--- TrainTicketsServer/TrainTicketsServer/Models/Entities/SeatInfo.cs
+++ TrainTicketsServer/TrainTicketsServer/Models/Entities/SeatInfo.cs
@@ -2,7 +2,7 @@
 {
     public class SeatInfo
     {
-        public int seat_id { get; set; }
+        public int trip_id { get; set; }
         public int seat_number { get; set; }
         public double price { get; set; }
         public string seat_type { get; set; }
